17bc1613aaf31f8f4ae136916b205a0cc4f65c37
Rahees Ahmed <raheesahmed256@gmail.com>
2024-07-25 21:23:29 +0500

    Refactor SideBar component to use fragment syntax

--- app/components/SideBar.tsx
+++ app/components/SideBar.tsx
@@ -2,17 +2,19 @@ import React from 'react';
 
 const SideBar = ({ onNewChat }) => {
     return (
-        <div className="w-64 min-h-screen bg-brand-black text-white flex flex-col">
-            <ul className="flex-grow mt-4 space-y-2">
-                <li>
-                    <button onClick={onNewChat} className="block p-2 bg-brand-dark-light w-40 text-center mx-auto">
-                        New Chat
-                    </button>
+        <>
+            <div className="w-64 min-h-screen bg-brand-black text-white flex flex-col">
+                <ul className="flex-grow mt-4 space-y-2">
+                    <li>
+                        <button onClick={onNewChat} className="block p-2 bg-brand-dark-light w-40 text-center mx-auto">
+                            New Chat
+                        </button>
 
 
-                </li>
-            </ul>
-        </div>
+                    </li>
+                </ul>
+            </div>
+        </>
     );
 }
 
