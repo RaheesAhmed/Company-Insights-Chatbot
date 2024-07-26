80f9c804bf44c427a7c460a2e6e1da7593fddb08
Rahees Ahmed <raheesahmed256@gmail.com>
2024-07-27 02:08:03 +0500

    fixed Typescript errors

--- app/api/files/[fileId]/route.ts
+++ app/api/files/[fileId]/route.ts
@@ -1,7 +1,8 @@
 import { openai } from "@/app/openai";
+import { Request } from "openai/src/_shims/node-types.mjs";
 
 // download file by file ID
-export async function GET(_request, { params: { fileId } }) {
+export async function GET(request:Request, { params: { fileId } }: { params: { fileId: string } }) {
   const [file, fileContent] = await Promise.all([
     openai.files.retrieve(fileId),
     openai.files.content(fileId),
