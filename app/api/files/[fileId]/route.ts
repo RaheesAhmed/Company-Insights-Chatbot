a48d40ddddff6a1390e9cd5519460cfb0ba2d171
Rahees Ahmed <raheesahmed256@gmail.com>
2024-07-27 02:15:35 +0500

    refactor: Simplify file download route implementation

--- app/api/files/[fileId]/route.ts
+++ app/api/files/[fileId]/route.ts
@@ -1,8 +1,8 @@
 import { openai } from "@/app/openai";
-import { Request } from "openai/src/_shims/node-types.mjs";
+
 
 // download file by file ID
-export async function GET(request:Request, { params: { fileId } }: { params: { fileId: string } }) {
+export async function GET({ params: { fileId } }: { params: { fileId: string } }) {
   const [file, fileContent] = await Promise.all([
     openai.files.retrieve(fileId),
     openai.files.content(fileId),
